12e658a8ca59516e5ecb74fb2f6473a76d5d36f2
kholoud <kholoudayman791@gmail.com>
2023-06-16 09:53:59 -0700

    print with write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - entery point
  * description: not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
- * Return: equal 1
- */
+ * Return: always 0
+*/
 
 int main(void)
 {
-	write("and that piece of art is useful\n" - Dora Korpar, 2015-10-19");
+	write(2,"and that piece of art is useful\n" - Dora Korpar, 2015-10-19");
 	return (1);
+}
