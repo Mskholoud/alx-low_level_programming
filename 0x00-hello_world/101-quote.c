60dbe847d081e6203f0ec3df02f5b459bf77f5db
kholoud <kholoudayman791@gmail.com>
2023-06-16 06:26:36 -0700

    print without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ * description: not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
+ * Return: equal 1
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\n" - Dora Korpar, 2015-10-19");
+	return (1);
