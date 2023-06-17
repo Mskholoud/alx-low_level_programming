eb233c7796d3af9bfd7d2ce6b0511848e6c1286d
kholoud <kholoudayman791@gmail.com>
2023-06-16 19:06:24 -0700

    the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: prints the alphabet in lowercase
+ * Return: always 0 (success)
+*/
+
+
+int main(void)
+{
+int kh = 97;
+
+while (kh <= 122)
+{
+	putchar(kh);
+	kh++;
+}
+putchar('\nr);
+return (0);
+}
