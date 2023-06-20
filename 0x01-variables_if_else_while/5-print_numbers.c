47c2b9baaca7e13a10e85e3414a6b8412aa26542
kholoud <kholoudayman791@gmail.com>
2023-06-20 11:31:12 -0700

    printing alphabets

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: printing numbers
+  *Return: always 0
+*/
+int main(void)
+{
+int a = 48;
+while (a <= 57)
+{
+putchar (a);
+a++;
+}
+putchar('\n');
+return (0);
+}
