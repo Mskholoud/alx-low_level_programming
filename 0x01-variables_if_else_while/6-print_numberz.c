5887c7641187d108bb25456c7c42f50d8e44cbc2
kholoud <kholoudayman791@gmail.com>
2023-06-20 11:35:52 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+/**
+  *main - entry point
+  *Description: printing numbers
+  *Return: always 0
+*/
+int main(void)
+{
+int a = 48;
+while (a <= 57)
+{
+putchar (a);
+a++;
+}
+putchar('\n');
+return (0);
+}
