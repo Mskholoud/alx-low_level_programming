59c143c8e259ada365d577d67b6317b5b4a8b147
kholoud <kholoudayman791@gmail.com>
2023-06-24 15:03:21 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - is entry point
+ *
+ * description: The character to print
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+int i;
+char my_Putchar[] = "_putchar";
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(my_Putchar[i]);
+	}
+_putchar('\n')
+return (0);
+}
