358e889b5fc3dcf4efcd3cf72eb882e96ad05f79
kholoud <kholoudayman791@gmail.com>
2023-07-01 17:09:59 -0700

    prints the sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - write the sign
+ * Description: printing the sign of a number(+, -, 0)
+ * @n: input character
+ * Return: 1 if n is greater than zero
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else(n = 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+}
