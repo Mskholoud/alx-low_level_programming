befddb871c081c1d32131848e36f33984ee9c966
kholoud <kholoudayman791@gmail.com>
2023-07-02 18:41:04 -0700

    print from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ * Description: prints the numbers, from 0  to 9
+ * void: integer to be checked
+ * Return: is always 0
+*/
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+		putchar('x');
+		putchar('\n');
+}
